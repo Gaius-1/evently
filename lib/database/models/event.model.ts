5d081f7dce944706ecfcdaa9314d42546ef46937
squash-bit <agbofrederick56@gmail.com>
2023-12-20 11:29:16 +0000

    increase max size for description

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -26,7 +26,7 @@ const EventSchema = new Schema({
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
   isFree: { type: Boolean, default: false },
-  url: { type: String },
+  url: { type: String, required: false },
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
   organizer: { type: Schema.Types.ObjectId, ref: 'User' },
 })
