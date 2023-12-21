887df3492228adb0ee51d678f2099917ac4861a1
squash-bit <agbofrederick56@gmail.com>
2023-12-21 10:26:19 +0000

    improve upon footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image"
 import Link from "next/link"
+import Image from "next/image"
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="border-t">
+    <footer className="border-top">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <Link href='/'>
+        <Link href="/">
           <Image 
             src="/assets/images/logo.svg"
             alt="logo"
             width={128}
-            height={38}
-          />
+            height={38} />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>{`© ${currentYear} Evently. All Rights Reserved.`}</p>
       </div>
     </footer>
   )
