552f4d2836429052b90c0fdbb923211bd7e5c7de
squash-bit <agbofrederick56@gmail.com>
2023-12-21 11:24:21 +0000

    remove upcoming events

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -59,7 +59,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       {/* Events Organized */}
-      { isUserAllowed && (
+      {/* { isUserAllowed && (
         <><section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
             <div className="wrapper flex items-center justify-center sm:justify-between">
               <h3 className='h3-bold text-center sm:text-left'>Events Organized By You</h3>
@@ -82,7 +82,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
               totalPages={organizedEvents?.totalPages} 
             />
           </section></>
-      )}
+      )} */}
 
       <section className="wrapper my-8">
         <Collection 
