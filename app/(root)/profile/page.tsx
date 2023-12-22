9265dc9b91b593e14fd0c8a8106ae9e851007cc1
squash-bit <agbofrederick56@gmail.com>
2023-12-22 09:06:02 +0000

    fetch user data using Clerk to render Orders Section

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -6,30 +6,36 @@ import { IOrder } from '@/lib/database/models/order.model'
 import { SearchParamProps } from '@/types'
 import { auth, useUser } from '@clerk/nextjs'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
-  // const { user } = useUser();
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
+  const { user } = useUser();
+  const [userEmail, setUserEmail] = useState<string | undefined>(undefined);
 
-  // Replace with the actual array of allowed email addresses
-  // const allowedEmailsArray = ["agbofrederick56@gmail.com", "user2@example.com"];
-  // Extract the primary email address from user.primaryEmailAddress
-  // const primaryEmail: string | undefined = user?.primaryEmailAddress?.emailAddress;
+  // Fetch user data and set the email
+  useEffect(() => {
+    const fetchUserData = async () => {
+      // Use Clerk's user object to get user data
+      setUserEmail(user?.primaryEmailAddress?.emailAddress);
+    };
+
+    fetchUserData();
+  }, [user]);
 
   const ordersPage = Number(searchParams?.ordersPage) || 1;
-  // const eventsPage = Number(searchParams?.eventsPage) || 1;
-  // const UpcomingEventsPage = Number(searchParams?.page) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  const orders = await getOrdersByUser({ userId, page: ordersPage})
-
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
-  // const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
-  // const upcomingEvents = await getUpcomingEvents({ page: UpcomingEventsPage, limit: 6 })
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
-  // Check if the user's email is in the allowed emails array
-  // const isUserAllowed = primaryEmail && allowedEmailsArray.includes(primaryEmail);
+  // Specify the allowed email
+  const allowedEmail = "agbofrederick56@gmail.com";
+
+  // Check if the user's email matches the allowed email
+  const isUserAllowed = userEmail === allowedEmail;
 
   return (
     <>
@@ -57,10 +63,11 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           totalPages={orders?.totalPages}
         />
       </section>
-      
+
       {/* Events Organized */}
-      {/* { isUserAllowed && (
-        <><section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      {isUserAllowed && (
+        <>
+          <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
             <div className="wrapper flex items-center justify-center sm:justify-between">
               <h3 className='h3-bold text-center sm:text-left'>Events Organized By You</h3>
               <Button asChild size="lg" className="button hidden sm:flex">
@@ -81,36 +88,13 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
               urlParamName="eventsPage"
               totalPages={organizedEvents?.totalPages} 
             />
-          </section></>
-      )} */}
+          </section>
+        </>
+      )}
 
-      {/* Recent Events Organized */}
-      {/* <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className='h3-bold text-center sm:text-left'>Upcoming Events</h3>
-          <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">
-              View More
-            </Link>
-          </Button>
-        </div>
-      </section>
-      
-      <section className="wrapper my-8">
-        <Collection 
-          data={upcomingEvents?.data}
-          emptyTitle="No Upcoming Events"
-          emptyStateSubtext="Go create some now"
-          collectionType="Upcoming_Events"
-          limit={3}
-          page={UpcomingEventsPage}
-          urlParamName="eventsPage"
-          totalPages={upcomingEvents?.totalPages}
-        />
-      </section> */}
-
+      {/* Additional sections... */}
     </>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
